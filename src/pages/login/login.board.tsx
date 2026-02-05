5a4be0e70167013378a4e4cfd2a899ce52995384
darshandhaduk-lab <darshan.dhaduk@oftgen.com>
2026-02-05 16:28:27 +0530

    login

--- src/pages/login/login.board.tsx
+++ src/pages/login/login.board.tsx
@@ -6,6 +6,7 @@ const Login = () => {
     <div className="page-container">
       <div className="el-409111846a38"></div>
       <h1 className="el-89bbd7e04198">Login</h1>
+      <a href="#" className="el-555f601c4222">Link Text</a>
     </div>
   );
 };
