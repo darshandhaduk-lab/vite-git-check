c00c447c6601ef3fcadd42a733265b6c3141fdb9
darshandhaduk-lab <darshan.dhaduk@oftgen.com>
2026-02-05 16:17:29 +0530

    test

---
+++ src/pages/login/login.board.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return (
+    <div className="page-container">
+      <div className="el-409111846a38"></div>
+      <h1 className="el-89bbd7e04198">Login</h1>
+    </div>
+  );
+};
+
+export default Login;
