3da3329a43cfef4b6ab0c05a3add6af5654e5fc8
darshandhaduk-lab <darshan.dhaduk@oftgen.com>
2026-02-05 16:31:02 +0530

    ss

--- src/pages/login/login.board.tsx
+++ src/pages/login/login.board.tsx
@@ -8,6 +8,11 @@ const Login = () => {
       <h1 className="el-89bbd7e04198">Login</h1>
       <a href="#" className="el-555f601c4222">Link Text</a>
       <img src="src/assets/images/neon-merry-christmas-sign-on-a-christmas-tree-free-photo.jpg" alt="neon-merry-christmas-sign-on-a-christmas-tree-free-photo.jpg" className="el-efd6a2b7db78" />
+      <ul className="el-8c7682470a1f">
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+      </ul>
     </div>
   );
 };
