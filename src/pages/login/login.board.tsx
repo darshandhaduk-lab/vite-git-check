ade779938645a620eb50f2c04cd6a7da3a37bc0f
darshandhaduk-lab <darshan.dhaduk@oftgen.com>
2026-02-05 18:56:47 +0530

    qweq

--- src/pages/login/login.board.tsx
+++ src/pages/login/login.board.tsx
@@ -4,7 +4,6 @@ import './login.css';
 const Login = () => {
   return (
     <div className="page-container">
-      <div className="el-409111846a38"></div>
       <h1 className="el-89bbd7e04198">Login</h1>
       <a href="#" className="el-555f601c4222">Link Text</a>
       <img src="src/assets/images/neon-merry-christmas-sign-on-a-christmas-tree-free-photo.jpg" alt="neon-merry-christmas-sign-on-a-christmas-tree-free-photo.jpg" className="el-efd6a2b7db78" />
